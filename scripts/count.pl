5be7f745633cb76f09a8b8b612097f7a7c2ad9dc
Colin <colin@mcwessels.org>
2021-07-14 05:06:37 +0000

    Now uses POSIX and Date::Parse modules(libraries?) to handle incrementing the dates. They also are used to get the day of the week and output that.

--- scripts/count.pl
+++ scripts/count.pl
@@ -1,22 +1,31 @@
 #!/usr/local/bin/perl
 use strict;
 use warnings;
+use Date::Parse;
+use POSIX;
 
 #This program requires the 1:0:1 file and will count how many unique hardware identifiers are recieved.
 
 my %tallys; #Tallys holds the date as the key and the count as the value
 my @dates; #Array holding all dates to count
 my $total = 0; #just the total number of hwids in the time period
+my %dayofweek; #Holds the date in yyyymmdd format as key and the corresponding day of week as value
 
 print "Enter start date (yyyymmdd):"; #This block querys for the start and end dates and adds each day in the range to an array.
 my $Sdate = <STDIN>;
 chomp $Sdate;
+my $Stime = str2time($Sdate); #srt2time converts the date into unix timestamp $Stime
 print "Enter end date:";
 my $Edate = <STDIN>;
 chomp $Edate;
-while($Sdate <= $Edate) {
-	push @dates, $Sdate;
-	$Sdate = $Sdate +1;
+my $Etime = str2time($Edate);
+
+
+while($Stime <= $Etime) {
+	my $ymd = POSIX::strftime('%Y%m%d', localtime($Stime)); #converts $Stime back into yyyymmdd format
+	push @dates, $ymd;
+	$dayofweek{$ymd} = POSIX::strftime('%a', localtime($Stime)); #Assigns the day of week corresponding to $Stime to the hash
+	$Stime = $Stime +24*60*60; #Adds one day to unix timestamp
 }
 
 sub count {
@@ -36,13 +45,13 @@ sub count {
 	return scalar (keys %list); #scalar countes the length of array, keys returns an array of all the keys in list.
 }
 
-print '[Date] : [Count]';
+print '[Date] : [Day of week] : [Count]';
 print "\n";
 print "______________\n";
 foreach my $date (@dates) {
 	my $tally = count($date); #$tally and $tallys are very similar
 	$tallys{$date} = $tally; #Stores the count for that date in the hash
-	print "$date : $tally\n";
+	print "$date : $dayofweek{$date} : $tally\n";
 	$total = $total + $tally;
 }
 
