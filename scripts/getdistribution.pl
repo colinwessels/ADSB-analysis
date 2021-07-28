1dd2708c3b9aaf754ee72e8b5f8ef5d03f31a447
Colin <colin@mcwessels.org>
2021-07-28 05:47:02 +0000

    getalts.pl is kinda copied from count.pl. It reads the 3:1:0 file and returns all the altitude values. getdistribution.pl will read the results of that script and will return the distribution with a variable bin size. The file this script returns is used by the gnuplot script to make a chart.

---
+++ scripts/getdistribution.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+
+my $binsize = 1000;
+my @dist;
+while (<>) {
+	chomp;
+	next if ($_ eq "");
+	next if ($_ < 0);
+	my $bin = int($_/$binsize);
+	$dist[$bin]++;
+}
+
+for my $i (0..$#dist) {
+	$dist[$i] += 0;
+	printf "%d %d\n", $i*$binsize, $dist[$i];
+}
